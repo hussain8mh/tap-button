82bf9c027a25b1798c1a280a18dcd476416a1deb
Hussain RPi <mohammedhussain8.mh@gmail.com>
2018-10-30 10:44:54 +0000

    code cleaned

--- led.py
+++ led.py
@@ -1,19 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+
 BUTTON = 23 
-LED = 24
 CLICK_CONSTANT =  0.3
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED,GPIO.OUT)
 GPIO.setup(BUTTON,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-def left_click(button_state):
-  if button_state == False:
-    GPIO.output(LED,True)
-  else:
-    GPIO.output(LED,False)
 
 s = e = 0.0
 
@@ -22,20 +16,23 @@ while True:
   button_state =GPIO.input(BUTTON)
 #  print "Button state " , button_state
 
-#  time.sleep(0.1)
-#  left_click(button_state)
-
- 
+# This Condition is true when button is NOT pressed
   if button_state ==  True:
-        if e - s >=  CLICK_CONSTANT  :
-                print "right click"
-        elif (e - s < CLICK_CONSTANT and e-s > 0):
-                print "left click"
+
+	pressed_time = e-s
+        if pressed_time >=  CLICK_CONSTANT  :
+                print "right click ", pressed_time
+        elif ( pressed_time < CLICK_CONSTANT and pressed_time > 0):
+                print "left click ",pressed_time
 
         s = time.time() 
+
+
+# This Condition is true when button IS pressed
   if button_state == False:
         e = time.time()
 
   #to mitigate human errors in pressing
-  time.sleep(0.01)
+
+  time.sleep(0.1)
 
