fff0782d53ed867dd51733cbc73081ed69aec2ba
Hussain RPi <mohammedhussain8.mh@gmail.com>
2018-10-30 11:04:22 +0000

    command line args added

--- socket/cli.py
+++ socket/cli.py
@@ -1,8 +1,10 @@
-import socket                
+import socket
+import sys
+                
 s = socket.socket()          
 
 port = 1235               
-server_ip = '127.0.0.1'
+server_ip = sys.argv[1]
 
 s.connect((server_ip, port)) 
 
